c5011e4853abefad1f3c5cc3ef993b1f7f54bc18
Osmir <osmirmariano@uft.edu.br>
2017-09-04 16:02:47 -0300

    Identação do código

--- produtorconsumidorAssicrona/src/produtor.java
+++ produtorconsumidorAssicrona/src/produtor.java
@@ -9,43 +9,41 @@
  */
  import java.util.*;
 public class produtor  extends Thread  {
-   dadoscompartilhados dc;
+    dadoscompartilhados dc;
 
-   public produtor(dadoscompartilhados dc){
-     this.dc=dc;
-   }
-   public void run(){
+    public produtor(dadoscompartilhados dc){
+      this.dc=dc;
+    }
+    public void run(){
        int cont=0;
-       
-       while(cont < dc.numdados){
+        while(cont < dc.numdados){
          
-         /*while(dc.consumindo)
-            
-         {
-            //dc.produzindo=false;
-            try{
-                Thread.sleep((int)Math.random()*2);
-            }
-            catch(Exception e){}
-        }
-        */
-        //dc.produzindo=true;
-	//dc.consumindo=false;
-         System.out.println("comecei a produzir");
-         for(int i=0; i<dc.tam; i++){
-           if(dc.buffer[i]==-1){
-             //dc.buffer[i]=(int)(Math.random()*1000);
-                dc.buffer[i]=i+1;
-             cont++;
-             if(cont >= dc.numdados){
-                 break;
-             }
+            /*while(dc.consumindo)
+
+            {
+               //dc.produzindo=false;
+               try{
+                   Thread.sleep((int)Math.random()*2);
+               }
+               catch(Exception e){}
            }
-         }
+           */
+           //dc.produzindo=true;
+           //dc.consumindo=false;
+            System.out.println("comecei a produzir");
+            for(int i=0; i<dc.tam; i++){
+                if(dc.buffer[i]==-1){
+                    //dc.buffer[i]=(int)(Math.random()*1000);
+                    dc.buffer[i]=i+1;
+                    cont++;
+                    if(cont >= dc.numdados){
+                        break;
+                    }
+                }
+            }
             dc.produzindo=false;
-         dc.consumindo=true;
-		 System.out.println("terminei de produzir - cont="+cont);
-         
+            dc.consumindo=true;
+            System.out.println("terminei de produzir - cont="+cont);
        }
    }
 }
